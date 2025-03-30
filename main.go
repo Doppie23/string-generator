16ecd74b3ee9f92cf3522b804690b4541896401d
Daniel van Opstal <dvopstal05@gmail.com>
2025-03-30 21:14:11 +0200

    WIP parsing files

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"sort"
+	"strings"
 )
 
 const fileName = "test.gram"
@@ -38,19 +41,110 @@ func (g Grammar) GenerateRandomString(i Identifier) string {
 	return r
 }
 
-func main() {
+type IdentLoc struct {
+	loc    int
+	length int
+}
+
+func indexAll(s string, substr string) []int {
+	var r []int
+	offset := 0
+	for {
+		loc := strings.Index(s[offset:], substr)
+		if loc == -1 {
+			break
+		}
+		offset += loc
+		r = append(r, offset)
+		offset += len(substr)
+	}
+	return r
+}
+
+func newGrammarFromFile(filepath string) Grammar {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content := string(data)
+	lines := strings.Split(strings.ReplaceAll(content, "\r\n", "\n"), "\n")
+
+	t := make(map[string][]string)
+
+	// TODO: support `|` syntax
+	for _, line := range lines {
+		ps := strings.Split(line, "->")
+		if len(ps) != 2 {
+			log.Fatalln("Invalid grammar file") // TODO: better error message...
+		}
+		ident := strings.TrimSpace(ps[0])
+		rest := strings.TrimSpace(ps[1])
+
+		if v, ok := t[ident]; ok {
+			t[ident] = append(v, rest)
+		} else {
+			t[ident] = []string{rest}
+		}
+	}
+
+	idents := make([]string, len(t))
+
+	i := 0
+	for k := range t {
+		idents[i] = k
+		i++
+	}
+
 	g := make(Grammar)
 
-	g[Identifier("S")] = []Word{[]Value{CustomString("test")}, []Value{Identifier("S"), Identifier("S")}}
-	fmt.Println(g.GenerateRandomString("S"))
+	for _, ident := range idents {
+		v := t[ident]
+		key := Identifier(ident)
+		g[key] = make([]Word, 0)
+		for _, tw := range v {
+			identLocs := make([]IdentLoc, 0)
 
-	// data, err := os.ReadFile(fileName)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+			for _, id := range idents {
+				locs := indexAll(tw, id)
+				if len(locs) == 0 {
+					continue
+				}
+				for loc := range locs {
+					identLocs = append(identLocs, IdentLoc{loc, len(id)})
+				}
+			}
 
-	// m := make(map[string][]string)
+			if len(identLocs) == 0 {
+				g[key] = append(g[key], Word{CustomString(tw)})
+				continue
+			}
 
-	// content := string(data)
-	// lines := strings.Split(strings.ReplaceAll(content, "\r\n", "\n"), "\n")
+			sort.Slice(identLocs, func(i int, j int) bool {
+				return identLocs[i].loc <= identLocs[j].loc
+			})
+
+			word := make(Word, 0)
+
+			// if first indent loc is 0, we first need to get that
+			//
+			// than we look for the next word and get it if its there and look if there is another ident, if so grab it aswell
+			// repeat
+
+			g[key] = append(g[key], word)
+		}
+	}
+
+	return g
+}
+
+func main() {
+	// g := make(Grammar)
+
+	// g[Identifier("S")] = []Word{[]Value{CustomString("test")}, []Value{Identifier("S"), Identifier("S")}}
+	// fmt.Println(g.GenerateRandomString("S"))
+
+	g := newGrammarFromFile(fileName)
+	fmt.Print(g)
+
 }
